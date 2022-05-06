2bdc5f72bf7b5fb8cfe9e3011fd9ba2c6532c4a8
kavery1994 <kammin.avery@gmail.com>
2022-05-05 23:02:23 -0600

    finished my script

--- script.js
+++ script.js
@@ -26,9 +26,9 @@ var lowerCase = [
   "w",
   "x",
   "y",
-  "z"
-]
-var upperCase =  [
+  "z",
+];
+var upperCase = [
   "A",
   "B",
   "C",
@@ -54,20 +54,9 @@ var upperCase =  [
   "W",
   "X",
   "Y",
-  "Z"
-]
-var randomNum = [
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "0",
-]
+  "Z",
+];
+var randomNum = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 var specialChar = [
   "!",
   "@",
@@ -82,33 +71,69 @@ var specialChar = [
   "=",
   "<",
   ">",
-  "/"
-]
-var allAray = [lowerCase, upperCase, randomNum, specialChar]
+  "/",
+];
+var allArray = [];
+var charLength;
 // Write password to the #password input
 
-function generatePassword() {
-  var userInput = prompt("Enter desired password length - must be between 8-128 characters.")
-  var charLength = userInput;
-  if (charLength < 8 || charLength > 128) {
-    var charLength = alert("ERROR: must be between 8-128 characters.")
-    generatePassword()
-  } else {
-    generatePassword()
+function promptUser() {
+  
+  while (true) {
+    var userInput = prompt(
+      "Enter desired password length - must be between 8-128 characters."
+    );
+    charLength = Number(userInput);
+    if (charLength < 8 || charLength > 128) {
+      alert("ERROR: must be between 8-128 characters.");
+    } else {
+      break;
+    }
   }
-var lowLetter = confirm("Lowercase letters okay?")
-var upLetter = confrim("Uppercase letters okay?")
-var numsOkay = confirm("Numbers okay?")
-var specChar = confirm("Special Characters okay?")
+  while (true) {
+    var lowLetter = confirm("Lowercase letters okay?");
+    var upLetter = confirm("Uppercase letters okay?");
+    var numsOkay = confirm("Numbers okay?");
+    var specChar = confirm("Special Characters okay?");
+    if (
+      lowLetter == false &&
+      upLetter == false &&
+      numsOkay == false &&
+      specChar == false
+    ) {
+      alert("No characters selected, please select atleast one.");
+    } else {
+      break;
+    }
+  }
+  if (lowLetter) {
+    allArray.push.apply(allArray,lowerCase);
+  }
+  if (upLetter) {
+    allArray.push.apply(allArray,upperCase);
+  }
+  if (numsOkay) {
+    allArray.push.apply(allArray,randomNum);
+  }
+  if (specChar) {
+    allArray.push.apply(allArray,specialChar);
+  }
+  console.log(allArray)
+}
+promptUser();
+
+function generatePassword() {
+  var password = ""
+  for (var i = 0; i < charLength; i++){
+  password += allArray[Math.floor(Math.random()*allArray.length)]
+  } 
+  return password
 }
 
-
 function writePassword() {
-  var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  var password = generatePassword();
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
